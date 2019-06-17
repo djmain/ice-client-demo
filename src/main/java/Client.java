1b384015b72ba46eaa7bb324ea998ea64d394238
xiadijun <540045123@qq.com>
2019-06-17 17:46:30 +0800

    ice与spring集成

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -17,6 +17,8 @@ public class Client
         System.out.println("result1:" + result1);
 
 
+
+
         HelloServicePrx helloServicePrx2 = (HelloServicePrx) context.getBean("helloServicePrx2");
         String result2 = helloServicePrx2.hello("dj");
         System.out.println("result2:" + result2);
