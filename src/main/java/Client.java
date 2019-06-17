6507d65c7f9c0b8b351a91c7f1164b828386e4aa
djmain <djmain@126.com>
2019-06-17 18:28:28 +0800

    8

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -16,6 +16,7 @@ public class Client
         String result1 = helloServicePrx1.hello("dj");
         System.out.println("result1:" + result1);
 
+
         HelloServicePrx helloServicePrx2 = (HelloServicePrx) context.getBean("helloServicePrx2");
         String result2 = helloServicePrx2.hello("dj");
         System.out.println("result2:" + result2);
